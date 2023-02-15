94de6c9277bcf546e76b5aba6a890a04ece27b84
Leon <leon.achteresch@gmail.com>
2023-02-15 10:27:30 +0100

    Import lokal

--- SQL_Import.py
+++ SQL_Import.py
@@ -1,7 +1,12 @@
 import sqlite3
 import pandas as pd
+import csv
+import pathlib
+path = pathlib.Path('test.py').parent.resolve()
 
-data = pd.read_csv (r'C:\Users\lachteresch\Desktop\FeinstaubProjekt\sensor-data\2022-02-09_dht22_sensor_3660.csv')   
-df = pd.DataFrame(data)
-
-print(df)
+with open(f'{path}\sensor-data\\2022-02-09_dht22_sensor_3660.csv')as f:
+    reader = csv.reader(f, delimiter=';')
+    next(reader)  
+    for row in reader:
+        sensor_type = row[1]
+        print(sensor_type)
