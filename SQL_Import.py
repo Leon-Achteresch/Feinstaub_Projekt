095d82245cc9adf31a5e50925a0c9183b78dae83
Leon <leon.achteresch@gmail.com>
2023-02-15 10:45:27 +0100

    DB Changes

--- SQL_Import.py
+++ SQL_Import.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pandas as pd
 import csv
 import pathlib
 path = pathlib.Path('test.py').parent.resolve()
@@ -9,4 +8,4 @@ with open(f'{path}\sensor-data\\2022-02-09_dht22_sensor_3660.csv')as f:
     next(reader)  
     for row in reader:
         sensor_type = row[1]
-        print(sensor_type)
+
