0d50094df1a41a304d7833907bc6f56a021e5866
FSchluenkes <frederik@schluenkes.de>
2023-02-22 10:44:46 +0100

    Downleaded alles bis zum letzten Tag in der DB

--- download.py
+++ download.py
@@ -1,6 +1,10 @@
 import urllib.request
 import datetime
 import glob
+import sqlite3
+
+conn = sqlite3.connect("sensor-data.db")
+c = conn.cursor()
 
 BASE = "http://archive.luftdaten.info"
 
@@ -44,12 +48,38 @@ def getdays():
     one_day = datetime.timedelta(days=1)
     current_date = datetime.date.today()
     days = 0
-    file = len(glob.glob(f'sensor-data\\{current_date}*.csv'))
-    while len(glob.glob(f'sensor-data\\{current_date}*.csv')) <1 and days <= 365:
+
+    c.execute("SELECT timestamp FROM DHT22 ORDER BY timestamp DESC LIMIT 2", {}) 
+    conn.commit()
+    record = c.fetchone()
+    datetimestr = record[0]
+
+    #c.execute("DELETE FROM DHT22 WHERE timestamp =" + datetimestr, {}) 
+    #conn.commit()
+    
+
+    lastdate = datetime.date(int(datetimestr[0:4]), int(datetimestr[5:7]), int(datetimestr[8:10]))
+
+    while lastdate < current_date:
         days +=1
         current_date = datetime.date.today() - days * one_day
+
+    #file = len(glob.glob(f'sensor-data\\{current_date}*.csv'))
+    #while len(glob.glob(f'sensor-data\\{current_date}*.csv')) <1 and days <= 365:
+        #days +=1
+        #current_date = datetime.date.today() - days * one_day
+    
+    test = f"{lastdate}"
+    c.execute("DELETE FROM DHT22 WHERE timestamp LIKE :date", {'date':test + '%'}) 
+    conn.commit()
+    c.execute("DELETE FROM SDS011 WHERE timestamp > :date", {'date':test + '%'}) 
+    conn.commit()
+
     return days
 
 
 if __name__ == '__main__':
     download_days(getdays())
+    
+    conn.close()
+    
