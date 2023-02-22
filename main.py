4650779f646655621fc0f80a2236dd2d3d7146e5
Leon <leon.achteresch@gmail.com>
2023-02-22 09:27:27 +0100

    Oberfläche mit PyGT6

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton
+
+class MyForm(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Fenster-Layout erstellen
+        self.layout = QVBoxLayout()
+
+        # Label erstellen
+        self.label = QLabel("Name:")
+        self.layout.addWidget(self.label)
+
+        # Textfeld erstellen
+        self.textbox = QLineEdit()
+        self.layout.addWidget(self.textbox)
+
+        # Button erstellen
+        self.button = QPushButton("Submit")
+        self.button.clicked.connect(self.submit_clicked)
+        self.layout.addWidget(self.button)
+
+        # Fenster-Layout einrichten
+        self.setLayout(self.layout)
+
+    def submit_clicked(self):
+        name = self.textbox.text()
+        print("Hello,", name)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyForm()
+    form.show()
+    sys.exit(app.exec())
